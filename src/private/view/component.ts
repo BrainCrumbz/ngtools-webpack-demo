c9cad35a3b2734ca82b30a2a9aa11b8007b089c1
GiuseppePiscopo <GiuseppePiscopo@users.noreply.github.com>
2017-05-25 16:14:24 +0200

    refactor(private): mote HTML template to separate file

--- src/private/view/component.ts
+++ src/private/view/component.ts
@@ -2,17 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'private-area',
-  template: `
-    <header>Private area header</header>
-    <section class="container-fluid">
-      <div class="row">
-        <div class="col-md-2">Here goes the sidebar.</div>
-        <main class="col-md-10">
-          Here goes the main content.
-        </main>
-      </div>
-    </section>
-  `,
+  templateUrl: './component.html',
   styleUrls: ['./component.css'],
 })
 export class PrivateComponent {
